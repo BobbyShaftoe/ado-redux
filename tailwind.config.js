e4832308728abf2176dc5fb076c30e3bfe2f9c40
Jack Shaftoe <Kingsize.nix@outlook.com>
2024-09-08 02:34:04 +0800

    Add tailwind config file, cleanup style.css

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./views/**/*.templ",
+        "static/js/**/*.js",
+    ],
+    darkMode: 'class',
+    theme: {
+
+        extend: {
+            colors: {
+                'nav-blue': '#3187df',
+                'nav-blue-light': '#7db3ea',
+                'nav-blue-lightest': '#d5ebfa',
+                'nav-blue-dark': '#1d4e81',
+                'button-blue': 'rgb(21 30 48 / var(--tw-bg-opacity))',
+                'button-blue-mid': 'rgb(28 41 66 / var(--tw-bg-opacity))',
+                'button-blue-dark': 'rgb(11 15 24 / var(--tw-bg-opacity))',
+            }
+        },
+    },
+    plugins: [],
+}
+
